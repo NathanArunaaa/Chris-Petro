818b575ba2a716e8f7062d94dad43d672bc44d0b
ChrisVelmachos <chrisvelmachos@hotmail.com>
2024-02-03 16:19:00 -0500

    Got window size and colour working and a space for answers

--- Chris/app.py
+++ Chris/app.py
@@ -1,4 +1,15 @@
 from tkinter import *
 
 root = Tk()
+#sets title of window to wtv u want
+root.title("Chris's Calculator")
+#Set window size (I made it same size as league client)
+root.geometry("1024x576")
+#setting background colour
+root.configure(bg="#000000")
+
+#creating area for answer to go into
+answers = Label(root, width = 50, height=2,text="",font=("Gilmer_Heavy",30))
+answers.pack()
+               
 root.mainloop()
