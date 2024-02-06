ae7f76f648fa8d46a04cf6b1b45a73b2036b031a
ChrisVelmachos <chrisvelmachos@hotmail.com>
2024-02-05 19:39:01 -0500

    update

--- Chris/app.py
+++ Chris/app.py
@@ -11,5 +11,7 @@ root.configure(bg="#000000")
 #creating area for answer to go into
 answers = Label(root, width = 50, height=2,text="",font=("Gilmer_Heavy",30))
 answers.pack()
-               
+
+Button(root, text = "C", width = 5, height = 1, font = ("arial", 30, "bold"), bd = 1, fg = "#fff", bg = "#3697f5").place() 
+
 root.mainloop()
